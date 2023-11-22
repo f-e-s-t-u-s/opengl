468af39bd480c34d3a3867f9ded196d81de76acd
Festus Gitahi <festusgitahik@gmail.com>
2023-11-22 14:11:53 +0300

    added color

--- hut.c
+++ hut.c
@@ -4,37 +4,42 @@
 #include <GL/glut.h>
 #endif
 
-void display() {
+void display()
+{
   glClear(GL_COLOR_BUFFER_BIT);
 
   // (square)
   glBegin(GL_POLYGON);
-    glColor3f(1,0,0); 
-    glVertex3f(-0.8, -0.8, 0.0);
-    glVertex3f(-0.8, -0.2, 0.0);
-    glVertex3f(0.8, -0.2, 0.0);
-    glVertex3f(0.8, -0.8, 0.0);
+  // red
+  glColor3f(1.0, 0.0, 0.0);
+  glVertex3f(-0.8, -0.8, 0.0);
+  // green
+  glColor3f(0.0, 1.0, 0.0);
+  glVertex3f(-0.8, -0.2, 0.0);
+  // blue
+  glColor3f(0.0, 1.0, 1.0);
+
+  glVertex3f(0.8, -0.2, 0.0);
+  glVertex3f(0.8, -0.8, 0.0);
   glEnd();
 
   glBegin(GL_POLYGON);
-    glColor3f(0,1,0); 
-    glVertex3f(-0.8, -0.2, 0.0);
-    glVertex3f(0.8, -0.2, 0.0);
-    glVertex3f(0.0, 0.6, 0.0);
+  glColor3f(1.0, 0.0, 0.0);
+  glVertex3f(-0.8, -0.2, 0.0);
+  glColor3f(0.0, 1.0, 0.0);
+
+  glVertex3f(0.8, -0.2, 0.0);
+  glColor3f(0.0, 1.0, 1.0);
+
+  glVertex3f(0.0, 0.6, 0.0);
   glEnd();
 
-//   roof (rectangle)
-  glBegin(GL_POLYGON);
-    glColor3f(0,0,1);
-    glVertex3f(-0.9, -0.2, 0.0);
-    glVertex3f(0.9, -0.2, 0.0);
-    glVertex3f(0.0, 1.0, 0.0);
-  glEnd();
 
   glFlush();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
